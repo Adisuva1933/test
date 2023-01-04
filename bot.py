a7ef4063be69994975ffdbc1d88bcb132136f8f0
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-01-04 21:10:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,9 +46,9 @@ class Bot(Client):
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
         app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, 8080).start()
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, 8080).start()
 
     async def stop(self, *args):
         await super().stop()
